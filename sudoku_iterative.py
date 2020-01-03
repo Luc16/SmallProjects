22d8cf106b7e5813352c4fb703d91ce1c577cbf2
luc16 <luc.joffily.ribas@gmail.com>
2020-01-03 15:50:31 -0300

    finished implications

--- sudoku_iterative.py
+++ sudoku_iterative.py
@@ -69,83 +69,6 @@ def printB(board):
     print()
 
 
-def find_next(board):
-    for i in range(len(board)):
-        if board[i] == 0:
-            return i
-
-
-def make_implications(board, idx):
-    can_implicate = True
-    implicated = []
-    while can_implicate:
-        prev_board = board
-        for i in range(idx+1, len(board)):
-            nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            if board[i] == 0:
-                mult = i // 9
-                square = identify_square(mult, i)
-                for k in range(9):
-                    col = board[i - (9 * mult) + 9 * k]
-                    row = board[k + (9 * mult)]
-                    sqr = board[square[k]]
-                    if 0 != col and col in nums:
-                        nums.remove(col)
-                    if 0 != row and row in nums:
-                        nums.remove(row)
-                    if 0 != sqr and sqr in nums:
-                        nums.remove(sqr)
-            if len(nums) == 1:
-                implicated.append(i)
-                board[i] = nums[0]
-        if board == prev_board:
-            return implicated
-
-
-def delete_implications(board, implicated):
-    deleted = []
-    for i in implicated:
-        for l in i:
-            deleted.append(l)
-            board[l] = 0
-    deleted.sort()
-    if len(deleted) == 0:
-        deleted = [82]
-    return deleted
-
-
-def back_track_w_imp(board_in):
-    fixed = []
-    board = board_in
-    implicated = []
-    for i in range(len(board)):
-        if board[i] != 0:
-            fixed.append(i)
-    i = 0
-    while i < len(board):
-        i = find_next(board)
-        board[i] = valid(board[i], i, board)
-        if board[i] == 0:
-            while True:
-                # delete_implications(board, implicated)
-                implicated = []
-                if i == 0:
-                    return 'Impossible'
-                if i not in fixed:
-                    board[i] = 0
-                if ((i - 1) not in fixed) and valid(board[i - 1], i - 1, board) != 0 and i-1 != 0:
-                    i -= 2
-                    break
-                else:
-                    i -= 1
-        # implicated.append(make_implications(board, i))
-        print(implicated)
-        print(i)
-        printB(board)
-        i += 1
-    return board
-
-
 if __name__ == '__main__':
     start_time = time.time()
     brd_uper_duper_hard = [
@@ -184,9 +107,5 @@ if __name__ == '__main__':
     # printB(backTrack(brd))
     # print("Program took:", time.time() - start_time, "to run")
 
-    # start_time = time.time()
-    # print(back_track_w_imp(brd))
-    printB(back_track_w_imp(brd))
-    print("Program took:", time.time() - start_time, "to run")
 
 
