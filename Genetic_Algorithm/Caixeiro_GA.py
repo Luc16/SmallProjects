e29f090fe604332caf9e249037f89dea45d853d9
luc16 <luc.joffily.ribas@gmai.com>
2021-03-31 20:28:15 -0300

    Mutação por rearranjo adicionada

--- Genetic_Algorithm/Caixeiro_GA.py
+++ Genetic_Algorithm/Caixeiro_GA.py
@@ -5,9 +5,8 @@ import time
 
 class Brute:
     def __init__(self, num_cities):
-        self.moves = [0]
-        [self.moves.append(x) for x in range(1, num_cities)]
-        self.final_list = list(itertools.permutations(self.moves[1:]))
+        self.moves = [x for x in range(1, num_cities)]
+        self.final_list = list(itertools.permutations(self.moves))
         self.score = []
 
     @staticmethod
@@ -48,18 +47,18 @@ class Child:
         return ((p1[0] - p2[0])*(p1[0] - p2[0]) + (p1[1] - p2[1])*(p1[1] - p2[1]))**0.5
 
     def fit(self, cities, scores):
-        self.score = 0
-        for index, move in enumerate(self.moves):
-            if index < len(self.moves)-1:
-                self.score += self.dist(cities[move], cities[self.moves[index+1]])
-            else:
-                self.score += self.dist(cities[move], cities[0])
-        scores.append(1/self.score)
+        if self.score == 0:
+            for index, move in enumerate(self.moves):
+                if index < len(self.moves)-1:
+                    self.score += self.dist(cities[move], cities[self.moves[index+1]])
+                else:
+                    self.score += self.dist(cities[move], cities[0])
+            scores.append(1/self.score)
         # return self.score, self.moves
 
 
 class Solver:
-    def __init__(self, cities, num_children, tag="both"):
+    def __init__(self, cities, num_children, tag="both", final=6):
         self.num_children = num_children
         self.current_generation = [Child(len(cities)) for _ in range(num_children)]
         self.next_generation = []
@@ -68,16 +67,26 @@ class Solver:
         self.cities = cities
         self.tag = tag
         self.num_cities = len(cities)
+        self.final = final
         random.seed()
         self.keep_pct = 0.25
         self.limit = int(self.keep_pct * self.num_children)
         self.num_gens = 0
 
+    def just_mutate_shuffle(self):
+        for _ in range(self.limit, self.num_children):
+            chosen_child = random.choices(self.current_generation, weights=self.score_list, k=1)
+            new_child = Child(self.num_cities, chosen_child[0].moves[1:])
+            rands = [random.randint(1, self.num_cities), random.randint(1, self.num_cities)]
+            rands.sort()
+            random.shuffle(new_child.moves[rands[0]: rands[1]])
+            self.next_generation.append(new_child)
+
     def just_mutate(self):
         for _ in range(self.limit, self.num_children):
             chosen_child = random.choices(self.current_generation, weights=self.score_list, k=1)
             new_child = Child(self.num_cities, chosen_child[0].moves[1:])
-            for i in range(random.randint(1, self.num_cities)):
+            for _ in range(random.randint(1, self.num_cities)):
                 mutations = random.sample(range(1, self.num_cities), 2)
                 if mutations[0] != mutations[1]:
                     temp = new_child.moves[mutations[0]]
@@ -88,18 +97,25 @@ class Solver:
     def mutate(self):
         for _ in range(random.randint(0, self.num_children-self.limit)):
             chosen_child = random.choice(self.next_generation)
-            for i in range(random.randint(1, self.num_cities)):
+            for _ in range(random.randint(1, self.num_cities)):
                 mutations = random.sample(range(1, self.num_cities), 2)
                 if mutations[0] != mutations[1]:
                     temp = chosen_child.moves[mutations[0]]
                     chosen_child.moves[mutations[0]] = chosen_child.moves[mutations[1]]
                     chosen_child.moves[mutations[1]] = temp
 
+    def mutate_shuffle(self):
+        for _ in range(random.randint(0, self.num_children-self.limit)):
+            chosen_child = random.choice(self.next_generation)
+            rands = [random.randint(1, self.num_cities), random.randint(1, self.num_cities)]
+            rands.sort()
+            random.shuffle(chosen_child.moves[rands[0]: rands[1]])
+
     def crossover(self):
         for _ in range(self.limit, self.num_children//2 + 1):
             chosen_children = random.choices(self.current_generation, weights=self.score_list, k=2)
             new_children = [Child(self.num_cities, child.moves[1:]) for child in chosen_children]
-            for i in range(random.randint(1, self.num_cities//2)):
+            for _ in range(random.randint(1, self.num_cities//2)):
                 gene = random.randint(1, self.num_cities-1)
                 temp = new_children[0].moves[gene]
                 indexes = [new_children[0].moves.index(new_children[1].moves[gene]), new_children[1].moves.index(temp)]
@@ -130,7 +146,7 @@ class Solver:
     def run(self):
         while True:
             # fit
-            self.score_list = []
+            self.score_list = self.score_list[:self.limit]
             for child in self.current_generation:
                 child.fit(self.cities, self.score_list)
             # sort
@@ -139,17 +155,20 @@ class Solver:
             self.prev_scores.append(self.current_generation[0].score)
             self.num_gens += 1
             # check end
-            if self.end(6):
+            if self.end(self.final):
                 break
             # evolve
             self.select()
             if self.tag == "both":
                 self.crossover()
                 self.mutate()
+                # self.mutate_shuffle()  # -------> not Helping
             elif self.tag == "crossover":
                 self.crossover()
             elif self.tag == "mutate":
                 self.just_mutate()
+            elif self.tag == "mut_shuffle":
+                self.just_mutate_shuffle()
 
             self.current_generation = self.next_generation
             self.next_generation = []
@@ -157,23 +176,45 @@ class Solver:
 
 
 if __name__ == '__main__':
-    dots = [[1, 2], [4, 1], [3, 5], [2, 4], [6, 2], [9, 5], [16, 11], [34, 29]]
+    dots = [[1, 2], [4, 1], [3, 5], [2, 4], [6, 2], [9, 5], [16, 11], [34, 29], [9, 7]]
+
     brute = Brute(len(dots))
     time_start = time.time()
     brute_result = brute.run(dots)
     time_end = time.time()
+
     print("Brute force result:", brute_result)
     print("Brute force time:", time_end-time_start)
     print()
-    accuracy = 0
-    time_sum = 0
-    for _ in range(1000):
-        time_start = time.time()
-        result = Solver(dots, 6).run()
-        time_end = time.time()
-        time_sum += time_end-time_start
-        # print(result)
-        accuracy += 100 - 100*abs((brute_result[0]-result[1])/brute_result[0])
-        # print("Percentage:", 100 - 100*abs((brute_result[0]-result[1])/brute_result[0]), "%")
-    print("Accuracy:", accuracy/1000)
-    print("Time per solve (Genetic Algorithm):", time_sum/1000)
+
+    NUM_CHILDREN = 50
+    DIFFERENT_TESTS = 4
+
+    rounded_brute = round(brute_result[0], 6)
+    gens = [0 for _ in range(DIFFERENT_TESTS)]
+    accuracy = [0 for _ in range(DIFFERENT_TESTS)]
+    reached_best = [0 for _ in range(DIFFERENT_TESTS)]
+    times = [0 for _ in range(DIFFERENT_TESTS)]
+    tags = ["both",
+            "crossover",
+            "mutate",
+            "mut_shuffle"]
+
+    num_attempts = 5000
+    for _ in range(num_attempts):
+        for i in range(DIFFERENT_TESTS):
+            time_start = time.time()
+            result = Solver(dots, NUM_CHILDREN, tag=tags[i], final=12).run()
+            time_end = time.time()
+            times[i] += time_end-time_start
+            gens[i] += result[0]
+            accuracy[i] += 100 - 100*abs((brute_result[0]-result[1])/brute_result[0])
+            if round(result[1], 6) == rounded_brute:
+                reached_best[i] += 100
+
+    for i in range(DIFFERENT_TESTS):
+        print("Accuracy (", tags[i], "): ", accuracy[i]/num_attempts, sep='')
+        print("Reached best:", reached_best[i] / num_attempts)
+        print("Time per solve (Genetic Algorithm):", times[i]/num_attempts)
+        print("Generations per solve (Genetic Algorithm):", gens[i] / num_attempts)
+        print()
